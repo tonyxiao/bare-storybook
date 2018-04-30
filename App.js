b05bc5afe1b76e63c050158efccefb5262c1a577
Tony Xiao <tonyx.ca@gmail.com>
2018-04-30 08:42:37 +0700

    Adding storybook via getstorybook

--- App.js
+++ App.js
@@ -1,13 +1,12 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import StorybookUIHMRRoot from "./storybook";
 
-export default class App extends React.Component {
+export class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <Text>A lot faster?</Text>
       </View>
     );
   }
@@ -16,8 +15,10 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
 });
+
+export default StorybookUIHMRRoot;
